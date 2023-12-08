ab85648a83b60c6acd9479fa22878391188b6cb1
秃头灯笼鱼 <ttdlyu@163.com>
2023-12-08 19:50:29 +0800

    feat(back): 获取并更新游戏最新版本号

--- src-tauri/src/functions/info.rs
+++ src-tauri/src/functions/info.rs
@@ -11,6 +11,35 @@ pub struct ConfigInfo {
     pub latest_version: u32,
 }
 
+fn get_latest_version() -> Result<u32,AnyError>{
+    let url = "https://forums.kleientertainment.com/game-updates/oni-alpha/";
+    let response = reqwest::blocking::get(url);
+    if response.is_err() {
+        return Ok(526233)
+    }
+    let body = response?.text().unwrap();
+    let document = scraper::Html::parse_document(&body);
+    let selector = Selector::parse("h3").expect("选择器无法解析");
+    let version_txt = document.select(&selector).next().map(|element| element.text().collect::<String>());
+    return if let Some(text) = version_txt {
+        let str = text.trim().replace("Release", "");
+        // TODO 这里的代码还要检查一下是否会直接报错
+        Ok(str.trim().parse::<u32>().unwrap())
+    } else {
+        Ok(526233)
+    }
+}
+
+pub fn refresh_version() -> Result<u32, AnyError> {
+    let latest_version = get_latest_version()?;
+    let mut config_info = load_config_file()?;
+    if &latest_version > &config_info.latest_version {
+        config_info.latest_version = latest_version;
+        save_config_file(&config_info)?;
+    }
+    Ok(latest_version)
+}
+
 fn load_config_file() -> Result<ConfigInfo, AnyError>{
     let config_path = Path::new("./config.json");
     let mut config_info = ConfigInfo{
@@ -45,5 +74,11 @@ mod test{
         print!("{:?}{:?}", config_info, result);
         assert_eq!(config_info.is_ok(), true);
         assert_eq!(result.is_ok(), true);
+    }
+    #[test]
+    fn get_latest_version_test(){
+        let result = refresh_version();
+        println!("{:?}",result);
+        assert_eq!(result.is_ok(),true)
     }
 }
